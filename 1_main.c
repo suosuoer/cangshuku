ebb809b9c4b3f1dd544d72bea84affe7f8625838
suosuoer <1482992616@qq.com>
2025-10-25 14:20:11 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,22 @@
-//
-//
-//
-#include <stdio.h>
+//202510603309
+//1482992616@qq.com
+//文嘉颖
+include <stdio.h> 
+
 int main()
 {
-    printf("Hello World");
-    return 0;
+     float celsius, fashrenheit;
+
+     
+     printf("请输入摄氏温度：");
+     scanf("%f", &celsius);
+
+     fashrenheit=celsius*9/5+32;
+
+     printf("转换后的华氏温度为: %.1f\n", fashrenheit);
+
+
+     return 0;
 }
+
+
