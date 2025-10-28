b4f0d5f9417e599c30d74f186cee617e5a4180f3
suosuoer <1482992616@qq.com>
2025-10-28 22:12:47 +0800

    Implement key safety check for prime numbers
    Added a prime number safety check for user input key.

--- 1_main.c
+++ 1_main.c
@@ -1,22 +1,34 @@
 //202510603309
 //1482992616@qq.com
 //文嘉颖
-#include <stdio.h> 
+#include <stdio.h>
 
 int main()
 {
-     float celsius, fashrenheit;
-
-     
-     printf("请输入摄氏温度：");
-     scanf("%f", &celsius);
-
-     fashrenheit=celsius*9/5+32;
-
-     printf("转换后的华氏温度为: %.1f\n", fashrenheit);
-
-
-     return 0;
+    int n;
+    int is_prime = 1;
+    printf ("请输入一个小于50的正整数作为密钥:");
+    scanf("%d", &n);
+    if(n<=0||n>=50){
+        printf("密钥不安全，请重新输入\n");
+        return 0;
+        if(n==1){
+            is_prime =0;
+        }
+    }else{
+        for(int i=2;i<=n/2;i++){
+            if(n % i ==0){
+                is_prime=0;
+                break;
+            }
+        }
+    }
+    if(is_prime){
+        printf("密钥安全\n");
+    }else{
+        printf("密钥不安全,请重新输入\n");
+    }
+    return 0;
 }
 
 
