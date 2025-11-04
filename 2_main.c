7e781d36befa8b5bf50dd67044bbf1a493379523
suosuoer <1482992616@qq.com>
2025-11-04 22:46:42 +0800

    Refactor main function to handle 3x3 matrix input
    Replaced the original logic for finding narcissistic numbers with a 3x3 matrix input and transposition logic.

--- 2_main.c
+++ 2_main.c
@@ -3,31 +3,44 @@
 //文嘉颖
 #include <stdio.h>
 
-int main(void)
-{
-    int num = 100;
-    int count = 0;
+int main() {
+    int arr[3][3];
+    int one[3][3];
+    int i = 0, j;
 
-    /* 遍历所有三位数，判断是否为水仙花数（每位的立方和等于自身） */
-    while (num <= 999) {
-        int hundreds = num / 100;
-        int tens = (num / 10) % 10;
-        int units = num % 10;
-        int sum = hundreds * hundreds * hundreds + tens * tens * tens + units * units * units;
 
-        if (sum == num) {
-            if (count == 0)
-                printf("%d", num);
-            else
-                printf(" %d", num);
-            count++;
+    while(i < 3) {
+        j = 0;
+        while(j < 3){
+            scanf("%d", &arr[i][j]);
+            j++;
+
         }
-
-        num++;
+        i++;
     }
 
-    printf("\n");
-    return 0;
-}
 
- 
+
+    i = 0;
+    while(i<3){
+        j = 0;
+        while(j<3){
+            one[j][i] = arr[i][j];
+            j++;
+        }
+        i++;
+    }
+    i = 0 ;
+    while(i<3){
+        j = 0;
+        while(j<3){
+        printf("%d", one[i][j]);
+        if(j<2) printf(" ");
+        
+        j++;
+    }
+    printf("\n");
+    i++;
+}
+return 0;
+}
