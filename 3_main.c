6953494a4623e3fc5b8a58df7b0b900dece918cf
suosuoer <1482992616@qq.com>
2025-11-05 22:38:22 +0800

    Refactor main function to sort an array of integers
    Replaced prime number checking logic with an array sorting implementation.

--- 3_main.c
+++ 3_main.c
@@ -3,41 +3,33 @@
 //文嘉颖
 #include <stdio.h>
 
-int main() {
-    int n;
-    int is_prime = 1;  
-    int i = 2;         
+int main(){
+    int arr[10];
+    int i,j,one;
 
-    
-    printf("请输入一个小于50的正整数作为密钥：");
-    scanf("%d", &n);
-
-    
-    if (n <= 0 || n >= 50) {
-        printf("密钥不安全，请重新输入\n");
-        return 0;
+    printf("请输入10个整数:");
+    for (i = 0; i < 10; i++){
+        scanf("%d", &arr[i]);
     }
 
-    
-    if (n == 1) {
-        is_prime = 0;
-    } else {
-        
-        while (i < n) {
-            if (n % i == 0) { 
-                is_prime = 0;
-                break;  
+
+    for(i = 0; i<9; i++){
+        for(j = 0; j < 9 - i; j++){
+            if(arr[j] > arr[j+1]){
+                one = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = one;
             }
-            i++;  
         }
     }
 
-    
-    if (is_prime) {
-        printf("密钥安全，密码设置成功\n");
-    } else {
-        printf("密钥不安全，请重新输入\n");
+    printf("排序后(从小到大):");
+    for(i = 0; i < 10; i++){
+        printf("%d", arr[i]);
+        if (i<9)printf(" ");
     }
+
+    printf("\n");
 
     return 0;
 }
