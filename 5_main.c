9858b4bef86ce527c3cb0195051a0f2930654e1f
suosuoer <1482992616@qq.com>
2025-11-06 21:17:23 +0800

    Update main function and add sum calculation
    Refactor main function to calculate sum using one function.

--- 5_main.c
+++ 5_main.c
@@ -2,25 +2,15 @@
 //1482992616@qq.com
 //文嘉颖
 #include <stdio.h>
-int main()
-{
-    int arr[5];
-    int i = 0;
-    while(i < 4){
-        scanf("%d", &arr[i]);
-        i++;
-    }
-    arr[4] = arr[0] + arr[1] + arr[2] + arr[3];
-    i = 0;
-    while(i < 5){
-        printf("%d", arr[i]);
-        if(i<4){
-            printf(" ");
-        }
-        i++;
-    }
-
-
-
+int main(){
+    int sum = one(1,100,1);
+    printf("%d",sum);
     return 0;
+}
+
+
+
+int one(int a1,int an,int step){
+    int n = (an - a1)/step + 1;
+    return (a1+an) * n /2;  
 }
