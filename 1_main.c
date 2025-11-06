c8ac00602a6931bdfabb805e41b17bd5a1b7d8ac
suosuoer <1482992616@qq.com>
2025-11-06 22:41:57 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -2,36 +2,25 @@
 //1482992616@qq.com
 //文嘉颖
 #include <stdio.h>
-int main(){
-    int arr[3][3];
-    int i = 0, j;
-    printf("请输入3*3矩阵:\n");
-    while(i<3){
-        j = 0;
-        while(j<3){
-            scanf("%d",&arr[i][j]);
-            j++;
+
+int main() {
+    int arr[3][3];  
+    int i, j;
+
+    
+    for (i = 0; i < 3; i++) {  
+        for (j = 0; j < 3; j++) { 
+            scanf("%d", &arr[i][j]);
         }
-        i++;
     }
 
-    printf("输出的3*3矩阵为:\n");
-    i = 0;
-    while(i<3){
-        j = 0;
-        while(j<3){
-            printf("%d",arr[i][j]);
-            if(i<3){
-                printf(" ");
-            }
-            j++;
+    
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);  
         }
-        printf("\n");
-        i++;
+        printf("\n");  
     }
+
     return 0;
 }
-
-
-
-       
