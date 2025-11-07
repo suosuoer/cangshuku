b1c67d742d2510a49fa216487cb2cf49de7a4301
suosuoer <1482992616@qq.com>
2025-11-07 23:15:37 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,37 +4,29 @@
 #include <stdio.h>
 
 int main() {
-    int arr[3][3];  
+    int arr[3][3];
     int i, j;
 
-    
-    for (i = 0; i < 3; i++) {  
-        for (j = 0; j < 3; j++) { 
+   
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
 
-    
+   
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
-            printf("%d ", arr[i][j]);  
+            printf("%d", arr[i][j]); 
+            
+
+            if (j < 2) { 
+                printf(" "); 
+            }
         }
-        printf("\n");  
+        printf("\n");
     }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
