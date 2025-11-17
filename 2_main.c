7673fbf3c408b3b07a3e8996dfc668a7d5b07dd6
suosuoer <1482992616@qq.com>
2025-11-17 23:23:19 +0800

    Update 2_main.c

--- 2_main.c
+++ 2_main.c
@@ -2,45 +2,23 @@
 //1482992616@qq.com
 //文嘉颖
 #include <stdio.h>
-
-int main() {
-    int arr[3][3];
-    int one[3][3];
-    int i = 0, j;
-
-
-    while(i < 3) {
-        j = 0;
-        while(j < 3){
-            scanf("%d", &arr[i][j]);
-            j++;
+int main(){
+    int arr[5];
+    int *p;
+    for(int i = 0;i < 5;i++){
+        scanf("%d", &arr[i]);
+    }
+    p = arr;
+    for(int i = 0;i < 5;i++){
+        *(p + i ) *= 2;
+    }
+    for(int i = 0;i < 5;i++){
+        if(i == 4){
+            printf("%d\n",arr[i] );
+        }else {
+            printf("%d ", arr[i]);
 
         }
-        i++;
     }
-
-
-
-    i = 0;
-    while(i<3){
-        j = 0;
-        while(j<3){
-            one[j][i] = arr[i][j];
-            j++;
-        }
-        i++;
-    }
-    i = 0 ;
-    while(i<3){
-        j = 0;
-        while(j<3){
-        printf("%d", one[i][j]);
-        if(j<2) printf(" ");
-        
-        j++;
-    }
-    printf("\n");
-    i++;
-}
-return 0;
+    return 0;
 }
