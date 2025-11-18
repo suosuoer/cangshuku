b481a1c72027c3a940b2a03f694a3e7afcde042c
suosuoer <1482992616@qq.com>
2025-11-18 21:33:48 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,34 +2,17 @@
 //1482992616@qq.com
 //文嘉颖
 #include <stdio.h>
-
-int main(){
-    int arr[10];
-    int i,j,one;
-
-    printf("请输入10个整数:");
-    for (i = 0; i < 10; i++){
-        scanf("%d", &arr[i]);
-    }
-
-
-    for(i = 0; i<9; i++){
-        for(j = 0; j < 9 - i; j++){
-            if(arr[j] > arr[j+1]){
-                one = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = one;
-            }
-        }
-    }
-
-    printf("排序后(从小到大):");
-    for(i = 0; i < 10; i++){
-        printf("%d", arr[i]);
-        if (i<9)printf(" ");
-    }
-
-    printf("\n");
-
+int main()
+{
+    int x , y;
+    scanf("%d %d",&x ,&y);
+    swap(&x, &y);
+    printf("%d %d\n",x,y);
     return 0;
+}
+void swap(int *a ,int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
 }
