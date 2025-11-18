385905139ad659e0266e78cd5139a8137a7a5db0
suosuoer <1482992616@qq.com>
2025-11-18 22:43:38 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -2,15 +2,25 @@
 //1482992616@qq.com
 //文嘉颖
 #include <stdio.h>
-int main(){
-    int sum = one(1,100,1);
-    printf("%d",sum);
-    return 0;
+void swap(int *ptr_arr,int len){
+    for(int i = len-1;i>0;i--){
+        *(ptr_arr + i) = *(ptr_arr + i - 1);
+    }
+    *ptr_arr = 0;
 }
-
-
-
-int one(int a1,int an,int step){
-    int n = (an - a1)/step + 1;
-    return (a1+an) * n /2;  
+int main(){
+    int arr[5];
+    int len = 5;
+    for(int i = 0;i<len;i++){
+        scanf("%d", &arr[i]);
+    }
+    swap(arr,len);
+    for(int i = 0;i<len;i++){
+        if(i == len -1){
+            printf("%d\n",arr[i]);
+        }else{
+            printf("%d ",arr[i]);
+        }
+    }
+    return 0;
 }
